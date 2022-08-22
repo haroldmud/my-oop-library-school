a8e1d6b818b35180df643f47aec1cd08f427921d
haroldmud <haroldmudosa@gmail.com>
2022-08-22 23:20:37 +0200

    Add instance variables

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,8 @@
 module Teacher 
   def initialize(specialization, age, parent_permission: true, name="Unknown")
-    
+    super(name, age, parent_permission)
+
+    @specialization = specialization
   end
 
   
