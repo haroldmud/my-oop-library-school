86114284860a8e3b8074c54c1cfdca0ca1b9f184
haroldmud <haroldmudosa@gmail.com>
2022-08-22 23:02:23 +0200

    create and iitialize the student module

---
+++ student.rb
@@ -0,0 +1,11 @@
+module Student
+    def initialize(classroom, age, parent_permission: true, name: 'Unknown')
+        super(name, age, parent_permission)
+        @classroom = classroom
+    end
+
+    def
+      "¯\(ツ)/¯"
+    end
+
+end
