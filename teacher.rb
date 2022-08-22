dbdc95491904d0ab32735b5e9c9fbaa21c4aa890
haroldmud <haroldmudosa@gmail.com>
2022-08-23 00:07:05 +0200

    Surrounding space missing added

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,6 @@
 module Teacher 
   def initialize(specialization, age, parent_permission: true, name: 'Unknown')
     super(name, age, parent_permission)
-
     @specialization = specialization
   end
 
