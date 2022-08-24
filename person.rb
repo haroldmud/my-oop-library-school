9b247022764e491c1c6264c12d630fd7f8d90b11
haroldmud <haroldmudosa@gmail.com>
2022-08-24 05:38:02 +0200

    Implement Super method to correct_name

--- person.rb
+++ person.rb
@@ -9,7 +9,8 @@ class Person < Nameable
   attr_reader :id
   attr_writer :name, :age, :parent_permission
   
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name, parent_permission: true)
+    super(self)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
