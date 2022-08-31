82653c27dcc90d454a2b956fa8a953296727586c
haroldmud <haroldmudosa@gmail.com>
2022-08-31 14:45:54 +0200

    Create book file

---
+++ book.rb
@@ -0,0 +1,12 @@
+class Book
+  attr_accessor :title, :author
+  
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+  
+  def add_rental(date)
+    new.Rental(date)
+  end
+end
