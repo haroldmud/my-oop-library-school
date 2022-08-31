3ae238b368adbe03d3668e34ff19a06d5b606740
haroldmud <haroldmudosa@gmail.com>
2022-08-31 14:55:17 +0200

    Update Student file

--- student.rb
+++ student.rb
@@ -1,14 +1,14 @@
 module Student
-    def initialize(classroom, age, parent_permission: true, name: 'Unknown')
-        super(name, age, parent_permission)
-        @classroom = classroom
-    end
+  def initialize(classroom, age, parent_permission: true, name: 'Unknown')
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-      "¯\(ツ)/¯"
-    end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 
-    def add_to_classroom
-      new.classroom(@classroom)
-    end
+  def add_to_classroom
+    new.classroom(@classroom)
+  end
 end
