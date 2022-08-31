8ded83b8ae197a855c9238129cd64b33e7f806bb
haroldmud <haroldmudosa@gmail.com>
2022-08-31 15:26:27 +0200

    Add add_student def

--- app.rb
+++ app.rb
@@ -10,4 +10,18 @@ class App
     @rental_arr = []
     @id = 0
   end
+
+  def add_student
+    printf 'name:'
+    name = gets.chomp
+    printf 'age:'
+    age = gets.chomp
+    printf 'permission parents? [Y/N]:'
+    permission = gets.chomp
+    true_permission = permission == 'y' || 'yes' || 'Y'
+    puts "Name: #{name} Age: #{age} created successfully"
+    @id += 1
+    @person_arr.push({ 'id' => @id.to_s, 'name' => name, 'age' => age, 'profession' => 'Student' })
+    Student.new('Year 1', age, name, 'Student', true_permission)
+  end
 end
