cb8c3f94241ae9adf7c7400e73fea121f3fa07d7
haroldmud <haroldmudosa@gmail.com>
2022-09-02 23:04:53 +0200

    describe teacher do

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,10 +1,16 @@
-require_relative '../book'
+require_relative '../teacher'
 
-describe Book do
-  context 'When testing the Book class' do
-    it 'should show a book object is an instance of book class' do
-      bk = Book.new('Percy Jackson', 'Rick Riordan')
-      expect(bk).to be_instance_of Book
+describe Teacher do
+  context 'When testing the Teacher class' do
+    it 'should show a teacher object is an instance of teacher class' do
+      tr = Teacher.new('Physics', 50, 'Simba', 'Teacher')
+      expect(tr).to be_instance_of Teacher
+    end
+
+    it "should say 'true' when we call the can_use_service? method" do
+      tr = Teacher.new('Physics', 24, 'Simba', 'Teacher')
+      message = tr.can_use_service?
+      expect(message).to be true
     end
   end
 end
